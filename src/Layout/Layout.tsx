382e4e01973649675ad3e7839e1acca8642dc61e
ImIshan7 <ishandanu999@gmail.com>
2024-07-19 11:49:54 +0530

    Layout Updated.

---
+++ src/Layout/Layout.tsx
@@ -0,0 +1,15 @@
+import {View, Text} from "react-native";
+import React from "react";
+
+const Layout = () => {
+    return (
+
+        <Stack
+            screenOptions={{
+                headerShown: false
+            }}
+        />
+    );
+};
+
+export default Layout

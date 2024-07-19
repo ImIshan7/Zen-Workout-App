32978a14ae2b836e4f00a042d63ee191ba5d851b
ImIshan7 <ishandanu999@gmail.com>
2024-07-19 21:20:08 +0530

    Fix Some Navigation Bugs

--- src/Components/BodyParts.jsx
+++ src/Components/BodyParts.jsx
@@ -1,62 +1,63 @@
-import { View, Text, FlatList, TouchableOpacity, Image } from 'react-native'
-import React from 'react'
-import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
+import { View, Text, FlatList, TouchableOpacity, Image } from 'react-native';
+import React from 'react';
+import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
 import { bodyParts } from '../Constants';
 import { LinearGradient } from 'expo-linear-gradient';
-import { useRouter } from 'expo-router';
+import { useNavigation } from '@react-navigation/native';
 import Animated, { FadeInDown } from 'react-native-reanimated';
 
 export default function BodyParts() {
-    const router = useRouter();
+    const navigation = useNavigation();
     return (
         <View className="mx-4 mt-56">
-            <Text style={{fontSize: hp(4)}} className="font-semibold text-black">
+            <Text style={{ fontSize: hp(4) }} className="font-semibold text-black">
                 Exercises
             </Text>
 
             <FlatList
                 data={bodyParts}
                 numColumns={2}
-                keyExtractor={item=> item.name}
+                keyExtractor={item => item.name}
                 showsVerticalScrollIndicator={false}
-                contentContainerStyle={{paddingBottom: 50, paddingTop: 20}}
+                contentContainerStyle={{ paddingBottom: 50, paddingTop: 20 }}
                 columnWrapperStyle={{
-                    justifyContent: 'space-between'
+                    justifyContent: 'space-between',
                 }}
-                renderItem={({item, index})=> <BodyPartCard router={router} index={index} item={item} />}
+                renderItem={({ item, index }) => <BodyPartCard navigation={navigation} index={index} item={item} />}
             />
         </View>
-    )
+    );
 }
 
-const BodyPartCard = ({item, router, index})=>{
+const BodyPartCard = ({ item, navigation, index }) => {
     return (
-        <Animated.View entering={FadeInDown.duration(400).delay(index*200).springify()}>
+        <Animated.View entering={FadeInDown.duration(400).delay(index * 200).springify()}>
             <TouchableOpacity
-                onPress={()=> router.push({pathname: '/..layout/Exercises', params: item})}
-                style={{width: wp(44), height: wp(52)}}
-                className="flex justify-end p-4 mb-4">
+                onPress={() => navigation.navigate('Exercises', item)}
+                style={{ width: wp(44), height: wp(52) }}
+                className="flex justify-end p-4 mb-4"
+            >
                 <Image
                     source={item.image}
-                    resizeMode='cover'
-                    style={{width: wp(44), height: wp(52)}}
+                    resizeMode="cover"
+                    style={{ width: wp(44), height: wp(52) }}
                     className="rounded-[35px] absolute"
                 />
                 <LinearGradient
                     colors={['transparent', 'rgba(0,0,0,0.9)']}
-                    style={{width: wp(44), height: hp(15)}}
-                    start={{x: 0.5, y: 0}}
-                    end={{x: 0.5, y: 1}}
+                    style={{ width: wp(44), height: hp(15) }}
+                    start={{ x: 0.5, y: 0 }}
+                    end={{ x: 0.5, y: 1 }}
                     className="absolute bottom-0 rounded-b-[35px]"
                 />
 
                 <Text
-                    style={{fontSize: hp(2.3)}}
+                    style={{ fontSize: hp(2.3) }}
                     className="text-white font-semibold text-center tracking-wide"
                 >
                     {item?.name}
                 </Text>
             </TouchableOpacity>
         </Animated.View>
-    )
-}
+    );
+};

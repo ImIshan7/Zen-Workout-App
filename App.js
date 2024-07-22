08555bcb21ff93f01078b42153be4d41e9c0d579
ImIshan7 <ishandanu999@gmail.com>
2024-07-22 10:05:39 +0530

    Api Created

--- App.js
+++ App.js
@@ -4,25 +4,29 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import MainScreen from './src/Screens/MainScreen.jsx';
 import HomeScreen from './src/Screens/HomeScreen.jsx';
+import BodyParts from './src/Components/BodyParts.jsx';
+import Exercises from './/src/Layout/Exercises.jsx';
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
-  return (
-      <GestureHandlerRootView style={{ flex: 1 }}>
-        <NavigationContainer>
-          <Stack.Navigator>
-            <Stack.Screen
-                name="Main"
-                component={MainScreen}
-                options={{ headerShown: false }}
-            />
-            <Stack.Screen
-                name="Home"
-                component={HomeScreen}
-            />
-          </Stack.Navigator>
-        </NavigationContainer>
-      </GestureHandlerRootView>
-  );
+    return (
+        <GestureHandlerRootView style={{ flex: 1 }}>
+            <NavigationContainer>
+                <Stack.Navigator initialRouteName="Main">
+                    <Stack.Screen
+                        name="Main"
+                        component={MainScreen}
+                        options={{ headerShown: false }}
+                    />
+                    <Stack.Screen
+                        name="Home"
+                        component={HomeScreen}
+                    />
+                    <Stack.Screen name="BodyParts" component={BodyParts} />
+                    <Stack.Screen name="Exercises" component={Exercises} />
+                </Stack.Navigator>
+            </NavigationContainer>
+        </GestureHandlerRootView>
+    );
 }
--- src/Layout/Exercises.jsx
+++ src/Layout/Exercises.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 
-export default function Exercises({ route }) {
-    const { params } = route;
+export default function Exercises() {
+
     return (
         <View>
-            <Text>{params.name}</Text>
-            {/* Render exercises for the selected body part */}
+            <Text> Hello World </Text>
+
         </View>
     );
 }
